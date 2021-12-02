fd0076b30e90176b739997a10e6cc0844ec522e1
Gabriel Issa <issag2016@gmail.com>
2021-12-02 12:51:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static("public"));
+
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/views/home-page.html')
+})
+
+
+app.get('/about', (request, response) => {
+    response.sendFile(__dirname + '/views/about-page.html')
+})
+app.get('/works', (request, response) => {
+    response.sendFile(__dirname + '/views/works-page.html')
+})
+
+
+
+
+
+app.listen(3000)
